08b200cc6422cc1f15862d98c16775aee71d7aa0
hape.k <hape.k@kakaocorp.com>
2023-01-02 17:18:12 +0900

    [구현] UI 및 글자 수 제한

---
+++ src/app.js
@@ -0,0 +1,33 @@
+const $inputTextArea = document.querySelector(".input-area__textarea");
+const $inputLetterCount = document.querySelector(".input-area__letter-count");
+
+const INPUT_TEXT_MAX_LENGTH = 10;
+
+const eventHandlers = {
+    onKeyDownTextArea(event) {
+        let contentText = event.target.value;
+        let isOver = false;
+
+        if (contentText.length > INPUT_TEXT_MAX_LENGTH) {
+            contentText = contentText.slice(0, INPUT_TEXT_MAX_LENGTH);
+            event.target.value = contentText;
+            isOver = true;
+        }
+        $inputLetterCount.innerHTML = `${contentText.length} / ${INPUT_TEXT_MAX_LENGTH}`;
+        if (isOver) {
+            $inputLetterCount.innerHTML += `<br>${INPUT_TEXT_MAX_LENGTH}자를 초과하여 작성할 수 없습니다.`;
+        }
+    },
+};
+const attachEventHandler = () => {
+    $inputTextArea.addEventListener("input", eventHandlers.onKeyDownTextArea);
+};
+function init() {
+    $inputLetterCount.innerHTML = `0 / ${INPUT_TEXT_MAX_LENGTH}`;
+    attachEventHandler();
+}
+
+function main() {
+    init();
+}
+main();
