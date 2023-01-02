2e0d78f42eb2e994280a6eeafb524cc3b043879e
hash.table <hash.table@kakaocorp.com>
2023-01-02 17:00:32 +0900

    chore: webpack config - plugins html-webpack-plugin 추가

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,6 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const path = require("path");
+
 module.exports = {
     mode: 'production',
     entry: path.resolve(__dirname, 'src/app.js'),
@@ -6,5 +8,11 @@ module.exports = {
         path: path.resolve(__dirname, 'dist'),
         filename: '[name].[contenthash].js',
         clean: true
-    }
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            filename: 'index.html',
+            template: './src/views/index.html'
+        })
+    ]
 }
