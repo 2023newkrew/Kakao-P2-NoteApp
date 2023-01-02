62479062d5e53db97b25e9b1fffb3468603c3f96
hash.table <hash.table@kakaocorp.com>
2023-01-02 18:57:42 +0900

    feat: header 이미지 및 nav 이미지 추가

---
+++ src/app.js
@@ -0,0 +1,12 @@
+// header images
+import './assets/menu.svg';
+import './assets/logo.svg';
+import './assets/notes.svg';
+import './assets/grid.png';
+import './assets/list.png';
+import './assets/user.png';
+
+// nav
+import './assets/notes.svg';
+import './assets/reminder.png';
+import './assets/settings.svg';
