d4cdc04b69d51989d9a9e2fc604ebf9ae63dc1fe
Kim Seung Hwan <dev.appmin@gmail.com>
2023-01-03 14:22:41 +0900

    fix: Import image error after webpack build, serve

--- src/app.js
+++ src/app.js
@@ -1,4 +1,26 @@
 import './styles/main.scss';
-import './styles/common.scss';
-import './styles/side-bar.scss';
-import './styles/header.scss';
+
+import listIcon from './assets/list.png';
+import menuIcon from './assets/menu-icon.svg';
+import notesIcon from './assets/notes.svg';
+import reminderIcon from './assets/reminder.png';
+import settingsIcon from './assets/settings.svg';
+import userIcon from './assets/user.png';
+
+const listIconEl = document.getElementById('listIcon');
+listIconEl.src = listIcon;
+
+const menuIconEl = document.getElementById('menuIcon');
+menuIconEl.src = menuIcon;
+
+const notesIconEl = document.getElementById('notesIcon');
+notesIconEl.src = notesIcon;
+
+const reminderIconEl = document.getElementById('reminderIcon');
+reminderIconEl.src = reminderIcon;
+
+const settingsIconEl = document.getElementById('settingsIcon');
+settingsIconEl.src = settingsIcon;
+
+const userIconEl = document.getElementById('userIcon');
+userIconEl.src = userIcon;
