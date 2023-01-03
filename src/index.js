d51364a78fa556fd64573bb69a46f962cc38aab6
kuma.koh <kuma.koh@kakaocorp.com>
2023-01-03 14:41:27 +0900

    refactor: 7-1 아키텍처 적용

--- src/index.js
+++ src/index.js
@@ -1,6 +1,1 @@
-import "./styles/reset.scss";
-import "./styles/global.scss";
-import "./styles/header.scss";
 import "./styles/main.scss";
-import "./styles/aside.scss";
-import "./styles/content.scss";
