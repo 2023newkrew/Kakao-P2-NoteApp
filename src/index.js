df01e83e582735fe0d4bafa82d2f1cf93fee9b17
kuma.koh <kuma.koh@kakaocorp.com>
2023-01-03 14:20:33 +0900

    style: 레이아웃, 스타일

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import "./styles/reset.scss";
+import "./styles/global.scss";
+import "./styles/header.scss";
+import "./styles/main.scss";
+import "./styles/aside.scss";
+import "./styles/content.scss";
