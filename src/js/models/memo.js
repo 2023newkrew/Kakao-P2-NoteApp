cff726acc77bdc842c345606a936d9c5900a3599
은승균 <dmstmdrbs98@gmail.com>
2023-01-04 18:31:12 +0900

    feat: 메모 데이터 관리를 위한 모델 추가

--- src/js/models/memo.js
+++ src/js/models/memo.js
@@ -1,16 +1,49 @@
+import {getItem, setItem} from '@utils/localStorage';
+import {nanoid} from 'nanoid';
+
+const transformObjectToInstance = memo => new Memo(memo.content, memo.id, memo.createdAt, memo.updatedAt);
+
 export default class Memo {
-  constructor(content) {
+  constructor(
+    content,
+    id = nanoid(),
+    createdAt = new Date().toLocaleString(),
+    updatedAt = new Date().toLocaleString()
+  ) {
     this.content = content;
-    this.createdAt = new Date().toLocaleString();
-    this.updatedAt = this.createdAt;
+    this.id = id;
+    this.createdAt = createdAt;
+    this.updatedAt = updatedAt;
+    this._saveMemo();
   }
-  getMemo() {
-    return {
-      ...this,
-    };
+  getMemo(id) {
+    const memos = Memo.getMemos();
+    return memos.find(memo => memo.id === id);
   }
-  setMemo(content) {
+  _saveMemo() {
+    const memos = getItem('memos') || [];
+    const currentMemoIndex = memos.findIndex(memo => memo.id === this.id);
+
+    if (currentMemoIndex === -1) {
+      memos.push({...this});
+    } else {
+      memos[currentMemoIndex] = {...this};
+    }
+    setItem('memos', memos);
+  }
+  updateMemo(content) {
     this.content = content;
     this.updatedAt = new Date().toLocaleString();
+
+    this._saveMemo();
+  }
+  static deleteMemo(id) {
+    const memos = Memo.getMemos();
+    const memoIndex = memos.findIndex(memo => memo.id === id);
+    memos.splice(memoIndex, 1);
+    setItem('memos', memos);
+  }
+  static getMemos() {
+    return (getItem('memos') || []).map(transformObjectToInstance);
   }
 }
