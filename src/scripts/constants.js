e6a5b05dc53033a6b3cd57bb22cc8869c23cc533
kuma.koh <kuma.koh@kakaocorp.com>
2023-01-05 14:44:03 +0900

    feat: 사이드바, 그리드/리스트뷰 토글 여부 저장

--- src/scripts/constants.js
+++ src/scripts/constants.js
@@ -1,1 +1,5 @@
 export const TRUE = "true";
+
+export const HIDDEN = "hidden";
+export const HIDDEN_ASIDE = "hiddenAside";
+export const LIST_VIEW = "listView";
--- src/scripts/header.js
+++ src/scripts/header.js
@@ -1,11 +1,16 @@
 import { toggleDataAttribute } from "./utils/dom";
+import { HIDDEN, HIDDEN_ASIDE, LIST_VIEW } from "./constants";
 
 function initToggleAsideButton() {
   const toggleAsideButtonEl = document.body.querySelector(".nav__button--toggle-aside");
   const asideEl = document.body.querySelector(".aside");
+  const hiddenAside = JSON.parse(localStorage.getItem(HIDDEN_ASIDE));
+
+  asideEl.dataset[HIDDEN] = hiddenAside;
 
   toggleAsideButtonEl.addEventListener("click", () => {
     toggleDataAttribute({ el: asideEl, attributeName: "hidden" });
+    localStorage.setItem(HIDDEN_ASIDE, asideEl.dataset[HIDDEN]);
   });
 }
 
@@ -14,11 +19,17 @@ function initToggleViewButton() {
   const listViewImgEl = toggleViewButtonEl.querySelector(".nav__img--list-view");
   const gridViewImgEl = toggleViewButtonEl.querySelector(".nav__img--grid-view");
   const memosEl = document.body.querySelector(".memos");
+  const listView = JSON.parse(localStorage.getItem(LIST_VIEW));
+
+  listViewImgEl.dataset[HIDDEN] = listView;
+  gridViewImgEl.dataset[HIDDEN] = !listView;
+  memosEl.dataset[LIST_VIEW] = listView;
 
   toggleViewButtonEl.addEventListener("click", () => {
     toggleDataAttribute({ el: listViewImgEl, attributeName: "hidden" });
     toggleDataAttribute({ el: gridViewImgEl, attributeName: "hidden" });
     toggleDataAttribute({ el: memosEl, attributeName: "listView" });
+    localStorage.setItem(LIST_VIEW, memosEl.dataset[LIST_VIEW]);
   });
 }
 
