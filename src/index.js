01c2a47dd01655c250e9620abc9b1d5d158f8a0c
kuma.koh <kuma.koh@kakaocorp.com>
2023-01-05 14:44:03 +0900

    refactor: input 관련 함수들 memo.js로 이동

--- src/index.js
+++ src/index.js
@@ -1,9 +1,7 @@
 import { initHeader } from "./scripts/header";
 import { initMemo } from "./scripts/memo";
-import { initInput } from "./scripts/input";
 
 import "./styles/main.scss";
 
 initHeader();
 initMemo();
-initInput();
--- src/scripts/memo.js
+++ src/scripts/memo.js
@@ -1,14 +1,22 @@
 import { createElementFromHtml } from "./utils/dom";
 
-export function createMemoEl({ id, memo }) {
+function createMemoEl({ id, memo }) {
   return createElementFromHtml(`<article class="memo" data-id="${id}">
-  <div class="memo__text">
-    ${memo}
-  </div>
+  <div class="memo__text">${memo}</div>
   <div class="memo__buttons">
     <button class="memo__button memo__button--delete">삭제하기</button>
   </div>
 </artice>`);
+}
+
+function addMemo(memo) {
+  const inputEl = document.body.querySelector(".input");
+  const memos = JSON.parse(localStorage.getItem("memos")) || {};
+  const id = Date.now();
+
+  inputEl.insertAdjacentElement("beforebegin", createMemoEl({ id, memo }));
+  memos[id] = memo;
+  localStorage.setItem("memos", JSON.stringify(memos));
 }
 
 function removeMemo(id) {
@@ -18,7 +26,7 @@ function removeMemo(id) {
   localStorage.setItem("memos", JSON.stringify(memos));
 }
 
-export function initMemo() {
+function initMemos() {
   const memosEl = document.body.querySelector(".memos");
   const inputEl = document.body.querySelector(".input");
   const memos = JSON.parse(localStorage.getItem("memos")) || {};
@@ -36,4 +44,27 @@ export function initMemo() {
     memoEl.remove();
     removeMemo(id);
   });
+}
+
+function initInput() {
+  const textareaEl = document.body.querySelector(".input__textarea");
+  const currentLengthEl = document.body.querySelector(".input__current-length");
+
+  textareaEl.addEventListener("input", (event) => {
+    currentLengthEl.innerText = event.target.value.length;
+  });
+
+  textareaEl.addEventListener("keydown", (event) => {
+    if (event.key != "Enter" || event.shiftKey || event.isComposing) return;
+
+    event.preventDefault();
+    addMemo(event.target.value);
+    event.target.value = "";
+    currentLengthEl.innerText = 0;
+  });
+}
+
+export function initMemo() {
+  initMemos();
+  initInput();
 }
