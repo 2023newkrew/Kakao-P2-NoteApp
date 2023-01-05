56edf8dc43710bfb9ec9958c60a139da272fdb2b
kuma.koh <kuma.koh@kakaocorp.com>
2023-01-05 14:44:03 +0900

    refactor: 메모 추가/삭제 관련 함수 분리

--- src/scripts/memo.js
+++ src/scripts/memo.js
@@ -11,40 +11,58 @@ function createMemoEl({ id, memo }) {
 </artice>`);
 }
 
-function addMemo(memo) {
+function insertMemoEl({ id, memo }) {
   const inputEl = document.body.querySelector(".input");
-  const memos = loadData(MEMOS) || {};
-  const id = Date.now();
 
   inputEl.insertAdjacentElement("beforebegin", createMemoEl({ id, memo }));
+}
+
+function loadMemos() {
+  return loadData(MEMOS) || {};
+}
+
+function saveMemo({ id, memo }) {
+  const memos = loadMemos();
+
   memos[id] = memo;
   saveData({ key: MEMOS, data: memos });
 }
 
-function removeMemo(id) {
-  const memos = loadData(MEMOS) || {};
+function deleteMemo(id) {
+  const memos = loadMemos();
 
   delete memos[id];
   saveData({ key: MEMOS, data: memos });
 }
 
+function handleAddMemo(memo) {
+  const id = Date.now();
+
+  insertMemoEl({ id, memo });
+  saveMemo({ id, memo });
+}
+
+function handleRemoveMemo(memoEl) {
+  const id = memoEl.dataset.id;
+
+  memoEl.remove();
+  deleteMemo(id);
+}
+
 function initMemos() {
   const memosEl = document.body.querySelector(".memos");
-  const inputEl = document.body.querySelector(".input");
-  const memos = loadData(MEMOS) || {};
+  const memos = loadMemos();
 
   Object.entries(memos).forEach(([id, memo]) => {
-    inputEl.insertAdjacentElement("beforebegin", createMemoEl({ id, memo }));
+    insertMemoEl({ id, memo });
   });
 
   memosEl.addEventListener("click", (event) => {
     if (!event.target.classList.contains("memo__button--delete")) return;
 
     const memoEl = event.target.closest(".memo");
-    const id = memoEl.dataset.id;
 
-    memoEl.remove();
-    removeMemo(id);
+    handleRemoveMemo(memoEl);
   });
 }
 
@@ -58,9 +76,10 @@ function initInput() {
 
   textareaEl.addEventListener("keydown", (event) => {
     if (event.key != "Enter" || event.shiftKey || event.isComposing) return;
-
     event.preventDefault();
-    addMemo(event.target.value);
+
+    handleAddMemo(event.target.value);
+
     event.target.value = "";
     currentLengthEl.innerText = 0;
   });
