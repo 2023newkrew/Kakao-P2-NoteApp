a8ec88d10c0195d5190d4c4392f13763ed834f85
kuma.koh <kuma.koh@kakaocorp.com>
2023-01-05 14:44:03 +0900

    chore: webpack config prod, dev 분리

--- webpack.common.js
+++ webpack.common.js
@@ -2,15 +2,6 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  mode: "development",
-  devServer: {
-    static: {
-      directory: path.join(__dirname, "./"),
-    },
-    compress: true,
-    port: 3000,
-    open: true,
-  },
   entry: path.resolve(__dirname, "src/index.js"),
   output: {
     path: path.resolve(__dirname, "dist"),
