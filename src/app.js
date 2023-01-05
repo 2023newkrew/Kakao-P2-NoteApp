930f98befe96250dfc0934b2952a339a0117a6a5
Kim Seung Hwan <dev.appmin@gmail.com>
2023-01-05 15:15:24 +0900

    feat: Load binders at app.js

--- src/app.js
+++ src/app.js
@@ -1,1 +1,10 @@
+/* eslint-disable no-new */
+import MemoBinder from './js/binder/memo-binder';
+import MemoContentBinder from './js/binder/memo-content-binder';
+import MemoModeBinder from './js/binder/memo-mode-binder';
+
 import './styles/main.scss';
+
+new MemoBinder();
+new MemoContentBinder();
+new MemoModeBinder();
