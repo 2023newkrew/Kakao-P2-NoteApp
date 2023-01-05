b091da1774e76009fffb8682c08c7d531b9d5833
Kim Seung Hwan <dev.appmin@gmail.com>
2023-01-05 10:34:57 +0900

    feat: Implement adding memo content and title

--- src/js/constants/message.js
+++ src/js/constants/message.js
@@ -4,3 +4,5 @@ export const CANNOT_USE_ABSTRACT_CONSTRUCTOR =
 export const CANNOT_USE_ABSTRACT_METHOD = 'Method must be implemented';
 
 export const INVALID_VIEW_MODEL = 'Invalid ViewModel';
+
+export const NO_NAME_MEMO_TITLE = '(제목 없는 메모)';
--- src/js/memo-app.js
+++ src/js/memo-app.js
@@ -1,3 +1,4 @@
+import { NO_NAME_MEMO_TITLE } from './constants/message';
 import Memo from './model/memo';
 import MemoGridView from './view/memo-grid-view';
 import MemoGridViewModel from './viewmodel/memo-grid-view-model';
@@ -7,26 +8,34 @@ export default class MemoApp {
 
   #memoGridViewModel;
 
+  #titleInputEl;
+
+  #contentTextAreaEl;
+
   constructor() {
     this.#memoGridViewModel = new MemoGridViewModel();
     this.#memoGridView = new MemoGridView(this.#memoGridViewModel);
+    this.#titleInputEl = document.querySelector('.title-input');
+    this.#contentTextAreaEl = document.querySelector('.content-textarea');
 
-    this.#bindEvents();
+    this._bindEvents();
   }
 
-  #bindEvents() {
+  _bindEvents() {
     const submitButtonEl = document.querySelector('.submit-button');
     submitButtonEl.addEventListener(
       'click',
-      this.onAddNoteButtonClick.bind(this),
+      this.#onAddNoteButtonClick.bind(this),
     );
   }
 
-  onAddNoteButtonClick() {
-    const memo = new Memo({
-      title: 'hello world',
-      content: 'hello world',
-    });
+  #onAddNoteButtonClick() {
+    let title = this.#titleInputEl.value;
+    const content = this.#contentTextAreaEl.value;
+
+    if (title === '' || title === null) title = NO_NAME_MEMO_TITLE;
+
+    const memo = new Memo({ title, content });
 
     this.#memoGridViewModel.appendMemo(memo);
     this.#memoGridView.render();
