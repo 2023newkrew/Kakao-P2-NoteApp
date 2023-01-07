cde448e4c5fd5a8c171aaeea64ebe8596dea0f6a
gray-sin <gray.sin@kakaocorp.com>
2023-01-07 14:02:28 +0900

    snackBarContainer 컴포넌트 추가

--- src/components/app/index.js
+++ src/components/app/index.js
@@ -3,15 +3,21 @@ import './index.scss';
 import { createHeaderComponent } from '../header';
 import { createNavigatorComponent } from '../navigator';
 import { createMainComponentWithDefaultProps } from '../mainWithDefaultProps';
+import { createSnackBarContainerComponent } from '../snackBarContainer';
 
 const createAppComponent = () => {
   const documentFragment = document.createDocumentFragment();
 
+  // 지금과 같은 경우엔 app 컴포넌트가 이벤트 중개만 한다.
   document.body.addEventListener('note-view-set', ({ detail }) => {
     const noteViewSetEvent = new CustomEvent('note-view-set', { detail });
-
     headerComponent.documentFragment.dispatchEvent(noteViewSetEvent);
     mainComponent.documentFragment.dispatchEvent(noteViewSetEvent);
+  });
+
+  document.body.addEventListener('snack-bar-request', ({ detail }) => {
+    const snackBarRequestEvent = new CustomEvent('snack-bar-request', { detail });
+    snackBarContainerComponent.documentFragment.dispatchEvent(snackBarRequestEvent);
   });
 
   const headerComponent = createHeaderComponent();
@@ -22,6 +28,25 @@ const createAppComponent = () => {
 
   const mainComponent = createMainComponentWithDefaultProps();
   documentFragment.appendChild(mainComponent.documentFragment);
+
+  const snackBarContainerComponent = createSnackBarContainerComponent();
+  documentFragment.appendChild(snackBarContainerComponent.documentFragment);
+
+  document.body.dispatchEvent(new CustomEvent('snack-bar-request', {
+    detail: {
+      content: '안녕하세요',
+      buttonValue: '해해',
+      handleButtonClick: () => { console.log('clicked') },
+    },
+  }));
+
+  document.body.dispatchEvent(new CustomEvent('snack-bar-request', {
+    detail: {
+      content: '안녕하세요',
+      buttonValue: '해해',
+      handleButtonClick: () => { console.log('clicked') },
+    },
+  }));
 
   return { documentFragment };
 };
---
+++ src/components/snackBarContainer/index.js
@@ -0,0 +1,41 @@
+import className from './index.scss';
+
+import { createDocumentFragment } from '../../utils';
+
+const SNACK_BAR_DISPLAY_MS = 2000;
+
+const createSnackBarContainerComponent = () => {
+  const documentFragment = createDocumentFragment(`<div class="${className.snackBarContainer}"></div>`);
+  const snackBarContainerElement = documentFragment.querySelector(`.${className.snackBarContainer}`);
+
+  documentFragment.addEventListener('snack-bar-request', ({ detail }) => {
+    const documentFragment = createDocumentFragment(`<div class="${className.snackBar}">${detail.content}<button class="${className.button}">${detail.buttonValue}</button></div>`);
+    const snackBarElement = documentFragment.querySelector(`.${className.snackBar}`);
+    const snackBarButton = snackBarElement.querySelector(`.${className.button}`);
+
+    snackBarButton.addEventListener('click', detail.handleButtonClick);
+    snackBarContainerElement.appendChild(documentFragment);
+
+    let timeoutId = null;
+
+    const clearSnackBarTimeout = () => {
+      clearTimeout(timeoutId);
+    }
+
+    const setSnackBarTimout = () => {
+      timeoutId = setTimeout(() => {
+
+        snackBarContainerElement.removeChild(snackBarElement);
+
+      }, SNACK_BAR_DISPLAY_MS);
+    }
+
+    snackBarElement.addEventListener('mouseenter', clearSnackBarTimeout);
+    snackBarElement.addEventListener('mouseleave', setSnackBarTimout);
+    setSnackBarTimout();
+  });
+
+  return { documentFragment };
+};
+
+export { createSnackBarContainerComponent };
